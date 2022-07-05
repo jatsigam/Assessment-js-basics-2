38aa59095188b089419ac655e62aa2a291fee214
Jatou Gaye <jatougaye@gmail.com>
2022-07-05 06:12:30 -0400

    first commit

---
+++ figma.js
@@ -0,0 +1,2 @@
+
+// https://www.figma.com/file/9Rn0AV04tUFPzXjkk0AWdR/RESTAURANT?node-id=0%3A1
---
+++ stack.js
@@ -0,0 +1,199 @@
+Technology Stack by Company (Airbnb, Slack & Meta)
+
+/*
+
+Airbnb  **************************************************************************************
+
+*/
+
+Applications and Data
+●	Nginx
+●	JavaScript
+●	Java
+●	React
+●	MySQL
+●	Amazon EC2
+●	Amazon S3
+●	Redis
+●	Ruby
+●	Sass
+●	Rails
+●	Amazon RDS
+●	Hadoop
+●	Amazon Elasticache
+●	Amazon EBS
+●	Airflow
+●	Presto
+●	Druid
+●	Native Navigation
+●	Airpal
+ 
+Utilities
+●	Amazon CloudFront
+●	Twilio SendGrid
+●	Amazon Route 53
+●	Twilio
+●	Visual Website Optimizer
+●	Braintree
+●	Lottie
+●	Nexmo
+●	Urban Airship
+●	Aerosolve
+●	DeepLinkDispatch
+ 
+DevOps
+●	GitHub
+●	New Relic
+●	Webpack
+●	Sentry
+●	Vagrant
+●	Kibana
+●	Amazon CloudWatch
+●	Logstash
+●	Datadog
+●	Jest
+●	Chef
+●	Enzyme
+●	Apache Mesos
+●	SmartStack
+
+Business Tools
+●	Slack
+●	G Suite
+●	InVision
+●	Asana
+●	Superset
+●	React Sketch.app
+●	Assemblage
+ 
+Analytics
+●	Google Analytics
+●	Campaign Monitor 
+
+//Slack **************************************************************************************
+
+Web Client
+●	JavaScript
+●	ES6
+●	Electron
+ 
+Mobile Devices
+●	Java
+●	Kotlin
+●	Objective C
+●	Swift
+ 
+Backend/API
+●	PHP
+●	Hacklang
+●	HHVM
+●	HAProxy
+●	Consul
+ 
+Data Storage
+●	MySQL
+●	Vitess
+●	Memcached
+●	MCRouter
+●	Presto
+●	Spark
+●	Airflow
+●	Hadoop
+●	Kafka
+ 
+Search Service
+●	Java
+●	SolrCloud
+ 
+Real-Time Messaging/Communication
+●	WebSockets
+●	Java
+●	Go
+ 
+Services
+●	gRPC
+●	Thrift
+●	JSON-over-HTTP
+●	Elixir
+●	Node
+●	Kafka
+●	Redis
+ 
+Server
+●	Terraform
+●	Chef
+●	Kubernetes
+●	Prometheus
+●	ELK
+ 
+Hosting
+●	AWS 
+
+
+//Meta	**************************************************************************************
+
+
+
+Applications and Data
+●	PHP
+●	React
+●	GraphQL
+●	Memcached
+●	Cassandra
+●	Flux
+●	Tornado
+●	HHVM
+●	Relay
+●	Presto
+●	Hack
+●	RocksDB
+●	Prophet
+●	Prepack
+●	Buck
+●	McRouter
+●	Beringei
+●	Yoga
+●	C++
+●	Haskell
+●	Erlang
+●	HBase
+●	MariaDB
+●	Hadoop
+●	MySQL
+●	Swift
+ 
+Languages and Coding
+●	Linux
+●	Apache HTTP
+●	MySQL
+●	PHP
+●	JavaScript
+●	HTML
+●	CSS
+●	PHP
+●	ReactJS
+●	JSON
+●	AJAX
+ 
+Utilities
+●	Framer
+●	BitBar
+●	Docusaurus
+●	Origami
+●	Stetho
+●	Pop
+●	ENorm
+ 
+DevOps
+●	Jenkins
+●	Datadog
+●	Jest
+●	Chef
+●	Phabricator
+●	Nuclide
+●	Infer
+●	LogDevice
+ 
+Business Tools
+●	Confluence
+●	Campaign Monitor
